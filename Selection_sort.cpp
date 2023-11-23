f3afa3bb196cfa9c83d0b1c7087abecc04b32fb1
Meghaa K <140928258+Meghak1@users.noreply.github.com>
2023-11-23 19:06:17 +0530

    Create Selection_sort.cpp

---
+++ Selection_sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream.h>
+using namespace std;
+int selection_sort(int arr[],int n);
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    selection_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+int selection_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 2; i++)
+    {
+        int mini = i;
+        for (int j = i; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[mini])
+                mini = j;
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+/*    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }*/
+}
